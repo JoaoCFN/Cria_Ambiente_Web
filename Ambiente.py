a7d24afcd323f76ca65c04b83ca3f80e9d82358d
João Carlos Ferreira Neto <58020444+JoaoCFN@users.noreply.github.com>
2020-04-20 18:38:21 -0300

    Update Ambiente.py

--- Ambiente.py
+++ Ambiente.py
@@ -1,11 +1,15 @@
 import os
+# inicia o npm no projeto
 def npm(): os.system("npm init -y")
+# cria meu index.html
 def html(): os.system("copy NUL index.html")
+# cria a pasta js e o script.js e volta ao diretório raiz
 def js():
     os.mkdir("js")
     os.chdir("js")
     os.system("copy NUL script.js")
     os.chdir("../")
+# cria a pasta sass e o style.scss e volta ao diretório raiz
 def sass():
     os.mkdir("sass")
     os.chdir("sass")
