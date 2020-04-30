14b4a0e7ae993f39a776a525e0eaca24096a20f6
João Carlos Ferreira Neto <58020444+JoaoCFN@users.noreply.github.com>
2020-04-29 21:28:33 -0300

    Update Ambiente.py

--- Ambiente.py
+++ Ambiente.py
@@ -1,14 +1,18 @@
 import os
+
 # inicia o npm no projeto
 def npm(): os.system("npm init -y")
+
 # cria meu index.html
 def html(): os.system("copy NUL index.html")
+
 # cria a pasta js e o script.js e volta ao diretório raiz
 def js():
     os.mkdir("js")
     os.chdir("js")
     os.system("copy NUL script.js")
     os.chdir("../")
+
 # cria a pasta sass e o style.scss e volta ao diretório raiz
 def sass():
     os.mkdir("sass")
@@ -17,6 +21,8 @@ def sass():
     os.chdir("../")
     os.system("code .")
     os.system("node-sass --output-style compressed --watch sass/style.scss estilo.css")
+
+# Cria a pasta raiz do projeto
 def pasta_raiz():
     nome_correto = False
     while nome_correto == False:
@@ -27,10 +33,12 @@ def pasta_raiz():
             nome_correto = True
     os.mkdir(nome_projeto)
     os.chdir(nome_projeto)
+
 def main():
     pasta_raiz()
     npm()
     html()
     js()
     sass()
+    
 main()
